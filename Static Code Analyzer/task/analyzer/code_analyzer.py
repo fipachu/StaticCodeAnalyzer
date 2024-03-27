bd78e6a51ac98b5fa0a12c7272c5f935a32cda13
Filip Łapkiewicz <80906036+fipachu@users.noreply.github.com>
2024-03-27 19:38:18 +0100

    Solved stage 1

--- Static Code Analyzer/task/analyzer/code_analyzer.py
+++ Static Code Analyzer/task/analyzer/code_analyzer.py
@@ -1,1 +1,6 @@
-# write your code here
+path = input()  # No input prompt allowed lol
+with open(path, 'r') as file:
+    for n, line in enumerate(file, 1):
+        # print(f'line #{n}: {repr(line)}')
+        if len(line.rstrip()) > 79:
+            print(f'Line {n}: S001 Line length over 79 characters')
