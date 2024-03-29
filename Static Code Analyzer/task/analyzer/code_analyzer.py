9e2c28144f3dc406194fc6d1fa5ca7e69d58a3a1
Filip Łapkiewicz <80906036+fipachu@users.noreply.github.com>
2024-03-29 15:49:06 +0100

    Introduce main() function

--- Static Code Analyzer/task/analyzer/code_analyzer.py
+++ Static Code Analyzer/task/analyzer/code_analyzer.py
@@ -4,11 +4,16 @@ class Over79CharactersError(Exception):
         super().__init__(self.message)
 
 
-path = input()  # No input prompt allowed lol
-with open(path, "r") as file:
-    for n, line in enumerate(file, 1):
-        try:
-            if len(line.rstrip()) > 79:
-                raise Over79CharactersError(n)
-        except Over79CharactersError as err:
-            print(err)
+def main():
+    path = input()  # No input prompt allowed lol
+    with open(path, "r") as file:
+        for n, line in enumerate(file, 1):
+            try:
+                if len(line.rstrip()) > 79:
+                    raise Over79CharactersError(n)
+            except Over79CharactersError as err:
+                print(err)
+
+
+if __name__ == main():
+    main()
