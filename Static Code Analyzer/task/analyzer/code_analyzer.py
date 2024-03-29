925225d6a8c283f1a289771be9bb0b8fa397a46f
Filip Łapkiewicz <80906036+fipachu@users.noreply.github.com>
2024-03-29 16:42:21 +0100

    Introduce exceptions for codes S003 to S006

--- Static Code Analyzer/task/analyzer/code_analyzer.py
+++ Static Code Analyzer/task/analyzer/code_analyzer.py
@@ -16,6 +16,36 @@ class IndentationNotMultipleOf4Error(Exception):
         super().__init__(self.message)
 
 
+class UnnecessarySemicolonError(Exception):
+    def __init__(self, line_num):
+        self.message = error_message(
+            line_num, "S003", "Unnecessary semicolon after a statement"
+        )
+        super().__init__(self.message)
+
+
+class LessThenTwoSpacesError(Exception):
+    def __init__(self, line_num):
+        self.message = error_message(
+            line_num, "S004", "Less than two spaces before inline comment"
+        )
+        super().__init__(self.message)
+
+
+class TodoFoundError(Exception):
+    def __init__(self, line_num):
+        self.message = error_message(line_num, "S005", "TODO found")
+        super().__init__(self.message)
+
+
+class MoreThanTwoBlankLinesError(Exception):
+    def __init__(self, line_num):
+        self.message = error_message(
+            line_num, "S006", "More than two blank lines preceding a code line"
+        )
+        super().__init__(self.message)
+
+
 def check_for_over_79_characters_error(path):
     with open(path, "r") as file:
         for n, line in enumerate(file, 1):
