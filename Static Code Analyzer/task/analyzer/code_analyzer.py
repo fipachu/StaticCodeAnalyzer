dbc56c562a289715c8071ed275ad1bfc876f2091
Filip Łapkiewicz <80906036+fipachu@users.noreply.github.com>
2024-03-29 18:12:46 +0100

    Move line preprocessing to main
    Stage 2 complete.

--- Static Code Analyzer/task/analyzer/code_analyzer.py
+++ Static Code Analyzer/task/analyzer/code_analyzer.py
@@ -2,52 +2,8 @@ def error_message(line_num, error_code, description):
     return f"Line {line_num}: {error_code} {description}"
 
 
-class Over79CharactersError(Exception):
-    def __init__(self, line_num):
-        self.message = error_message(line_num, "S001", "Line length over 79 characters")
-        super().__init__(self.message)
-
-
-class IndentationNotMultipleOf4Error(Exception):
-    def __init__(self, line_num):
-        self.message = error_message(
-            line_num, "S002", "Indentation is not a multiple of four"
-        )
-        super().__init__(self.message)
-
-
-class UnnecessarySemicolonError(Exception):
-    def __init__(self, line_num):
-        self.message = error_message(
-            line_num, "S003", "Unnecessary semicolon after a statement"
-        )
-        super().__init__(self.message)
-
-
-class LessThenTwoSpacesError(Exception):
-    def __init__(self, line_num):
-        self.message = error_message(
-            line_num, "S004", "Less than two spaces before inline comment"
-        )
-        super().__init__(self.message)
-
-
-class TodoFoundError(Exception):
-    def __init__(self, line_num):
-        self.message = error_message(line_num, "S005", "TODO found")
-        super().__init__(self.message)
-
-
-class MoreThanTwoBlankLinesError(Exception):
-    def __init__(self, line_num):
-        self.message = error_message(
-            line_num, "S006", "More than two blank lines preceding a code line"
-        )
-        super().__init__(self.message)
-
-
 def line_over_79_characters(line, line_number):
-    if len(line.rstrip()) > 79:
+    if len(line) > 79:
         print(error_message(line_number, "S001", "Line length over 79 characters"))
 
 
@@ -88,7 +44,6 @@ def todo_found(line, line_number):
 
 
 def more_than_two_blank_lines(line, line_number, blank_count):
-    line = line.rstrip()
     if len(line) == 0:
         blank_count += 1
     else:
@@ -109,6 +64,8 @@ def main():
     with open(path, "r") as file:
         blank_count = 0
         for n, line in enumerate(file, 1):
+            line = line.rstrip()
+
             line_over_79_characters(line, n)
             indentation_not_multiple_of_4(line, n)
             unnecessary_semicolon(line, n)
