4358b45e8e65ae3fefbf08291123a59468e00134
Filip Łapkiewicz <80906036+fipachu@users.noreply.github.com>
2024-03-29 15:47:12 +0100

    Introduce user-defined exception

--- Static Code Analyzer/task/analyzer/code_analyzer.py
+++ Static Code Analyzer/task/analyzer/code_analyzer.py
@@ -1,6 +1,14 @@
+class Over79CharactersError(Exception):
+    def __init__(self, line_num):
+        self.message = f"Line {line_num}: S001 Line length over 79 characters"
+        super().__init__(self.message)
+
+
 path = input()  # No input prompt allowed lol
-with open(path, 'r') as file:
+with open(path, "r") as file:
     for n, line in enumerate(file, 1):
-        # print(f'line #{n}: {repr(line)}')
-        if len(line.rstrip()) > 79:
-            print(f'Line {n}: S001 Line length over 79 characters')
+        try:
+            if len(line.rstrip()) > 79:
+                raise Over79CharactersError(n)
+        except Over79CharactersError as err:
+            print(err)
