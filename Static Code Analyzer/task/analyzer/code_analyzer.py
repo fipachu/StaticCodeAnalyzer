c54a2dfdce7e866eaa96f9b85fc6ac088ee35907
Filip Łapkiewicz <80906036+fipachu@users.noreply.github.com>
2024-03-29 16:31:06 +0100

    Extract error message generation

--- Static Code Analyzer/task/analyzer/code_analyzer.py
+++ Static Code Analyzer/task/analyzer/code_analyzer.py
@@ -1,12 +1,18 @@
+def error_message(line_num, error_code, description):
+    return f"Line {line_num}: {error_code} {description}"
+
+
 class Over79CharactersError(Exception):
     def __init__(self, line_num):
-        self.message = f"Line {line_num}: S001 Line length over 79 characters"
+        self.message = error_message(line_num, "S001", "Line length over 79 characters")
         super().__init__(self.message)
 
 
 class IndentationNotMultipleOf4(Exception):
     def __init__(self, line_num):
-        self.message = f"Line {line_num}: S002 Indentation is not a multiple of four"
+        self.message = error_message(
+            line_num, "S002", "Indentation is not a multiple of four"
+        )
         super().__init__(self.message)
 
 
